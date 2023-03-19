21292913e0865fdc1396dd0f26afb25313998150
Jessie Zhong <zhongjessie2003@gmail.com>
2023-03-18 21:45:50 -0700

    ADJUSTED TEST_MODEL

--- test_model.py
+++ test_model.py
@@ -41,16 +41,29 @@ def test_model_on_beatmap_id(beatmap_id, model_path, max_sequence_length, label_
 
     # Get the vectors and pad them
     beatmap_vectors = beatmap_data['vectors']
-    beatmap_vectors_padded = pad_sequences([beatmap_vectors], dtype='float32', padding='post', maxlen=max_sequence_length)
+    beatmap_vectors_trimmed = np.delete(beatmap_vectors, 3, axis=1)
+    beatmap_vectors_padded = pad_sequences([beatmap_vectors_trimmed], dtype='float32', padding='post', maxlen=max_sequence_length)
+
 
     # Make a prediction using the model
     prediction = model.predict(beatmap_vectors_padded)
 
-    # Convert the prediction to the original category label
-    predicted_category_numerical = np.argmax(prediction, axis=-1)
-    predicted_category = label_encoder.inverse_transform(predicted_category_numerical)
+    # Get the predicted category indices and their corresponding confidence scores
+    predicted_category_indices = np.argsort(-prediction, axis=-1)
+    predicted_category_confidences = np.sort(prediction, axis=-1)[:, ::-1]
+
+    # Get the labels for the first and second most confident predictions
+    predicted_category = label_encoder.inverse_transform(predicted_category_indices[:, 0])
+    second_predicted_category = label_encoder.inverse_transform(predicted_category_indices[:, 1])
+
+    # Get the confidence scores for the first and second most confident predictions
+    predicted_confidence = predicted_category_confidences[:, 0]
+    second_predicted_confidence = predicted_category_confidences[:, 1]
 
     print(f"Predicted category for beatmap ID {beatmap_id}: {predicted_category[0]}")
+    print(f"Confidence score for the predicted category: {predicted_confidence[0]}")
+    print(f"Second most confident prediction: {second_predicted_category[0]}")
+    print(f"Confidence score for the second most confident prediction: {second_predicted_confidence[0]}")
 
     os.unlink(temp_file_path)  # Delete the temporary file
 
@@ -61,8 +74,8 @@ if __name__ == "__main__":
         exit(1)
 
     beatmap_id = sys.argv[1]
-    model_path = "oracle/cnn_model.h5"
-    max_sequence_length = 2444  # Set this to the same value you used in the training script
+    model_path = "oracle/cnn_model_final.h5"
+    max_sequence_length = 6948  # Set this to the same value you used in the training script
     label_encoder_path = "oracle/label_encoder.pkl"  # Replace with the path to your saved LabelEncoder
 
     test_model_on_beatmap_id(beatmap_id, model_path, max_sequence_length, label_encoder_path)
